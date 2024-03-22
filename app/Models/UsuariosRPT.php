46d85e0b5ba525901b20e287ebc6b727648b26db
reportik <79289765+reportik@users.noreply.github.com>
2024-03-22 10:42:16 -0600

    fcm

--- app/Models/UsuariosRPT.php
+++ app/Models/UsuariosRPT.php
@@ -40,38 +40,41 @@ class UsuariosRPT extends Model {
         try {
         $credentialsFilePath = public_path('service_account.json');
         $client = new Google_Client();
-        
         $client->setAuthConfig($credentialsFilePath);
         $client->addScope('https://www.googleapis.com/auth/firebase.messaging');
-        $apiurl = 'https://fcm.googleapis.com/v1/projects/<PROJECT_ID>/messages:send';
+        $apiurl = 'https://fcm.googleapis.com/v1/projects/mi-nomina-iteknia/messages:send';
         $client->refreshTokenWithAssertion();
         $token = $client->getAccessToken();
         $access_token = $token['access_token'];
-
+        
         $headers = [
-            "Authorization: Bearer $access_token",
-            'Content-Type: application/json'
+            "Authorization: Bearer". $access_token,
+            "Content-Type: application/json"
         ];
         $test_data = [
             "title" => "TITLE_HERE",
             "description" => "DESCRIPTION_HERE",
         ];
-
+        
         $data['data'] =  $test_data;
-
+        
         $data['token'] = $fcm->fcm_token; // Retrive fcm_token from users table
-
+        
         $payload['message'] = $data;
         $payload = json_encode($payload);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $apiurl);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-            $res = curl_exec($ch);
-         curl_close($ch);
+        
+        $httpClient = $client->authorize();
+        $res = $httpClient->post($apiurl, ['json' => $payload]);
+    //     $ch = curl_init();
+    //     curl_setopt($ch, CURLOPT_URL, $apiurl);
+    //     curl_setopt($ch, CURLOPT_POST, true);
+    //     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    //    // dd($ch);
+    //        $res = curl_exec($ch);
+         //curl_close($ch);
         if ($res) {
             return response()->json([
                 'message' => 'Notification has been Sent'
