2429d4909e8520932a98e864558042802f261c57
reportik <79289765+reportik@users.noreply.github.com>
2024-03-22 10:44:28 -0600

    fcm

--- app/Models/UsuariosRPT.php
+++ app/Models/UsuariosRPT.php
@@ -63,17 +63,17 @@ class UsuariosRPT extends Model {
         $payload['message'] = $data;
         $payload = json_encode($payload);
         
-        $httpClient = $client->authorize();
-        $res = $httpClient->post($apiurl, ['json' => $payload]);
-    //     $ch = curl_init();
-    //     curl_setopt($ch, CURLOPT_URL, $apiurl);
-    //     curl_setopt($ch, CURLOPT_POST, true);
-    //     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    //     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    //     curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-    //    // dd($ch);
-    //        $res = curl_exec($ch);
+        //$httpClient = $client->authorize();
+       // $res = $httpClient->post($apiurl, ['json' => $payload]);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $apiurl);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+       // dd($ch);
+           $res = curl_exec($ch);
          //curl_close($ch);
         if ($res) {
             return response()->json([
