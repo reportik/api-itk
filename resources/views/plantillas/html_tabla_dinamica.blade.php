e5795d7105192b01de9ba13519ec2091bf836d91
reportik <79289765+reportik@users.noreply.github.com>
2024-05-13 00:05:40 -0600

    array

--- resources/views/plantillas/html_tabla_dinamica.blade.php
+++ resources/views/plantillas/html_tabla_dinamica.blade.php
@@ -1,14 +1,18 @@
 <table>
     <tr>
-        @foreach ($encabezados as $encabezado) 
-            <th> {{$encabezado}} </th>
+        @foreach ($encabezados as $encabezado)
+        @if (strpos($encabezado, 'rpt_') !== 0)
+        <th> {{$encabezado}} </th>
+        @endif
         @endforeach
     </tr>
-    @foreach ($filas as $fila) 
-        <tr>
-        @foreach ($fila as $dato) 
-            <td>{{$dato}}</td>';
+    @foreach ($filas as $fila)
+    <tr>
+        @foreach ($fila as $clave => $dato)
+        @if (strpos($clave, 'rpt_') !== 0)
+        <td>{{$dato}}</td>
+        @endif
         @endforeach
-        </tr>
+    </tr>
     @endforeach
 </table>
